f888e89c3ccb3d6376a8e8e21bf6b36973d16055
Eldin Kecap <eldinkecap@gmail.com>
2022-09-18 23:09:34 +0200

    implemented dynamic header with js

---
+++ javascript/header.js
@@ -0,0 +1,14 @@
+headerContainer.innerHTML = `<div class="headerBar">
+<div class="logo">
+    <img class = "hotelIcon" src="./images/Hotel-icon.png" alt="hotelIcon">
+    <div class = "hotelIconCaption">Hotel</div>
+</div>
+<div class="nav">
+    <ul class="navList">
+        <li class="navListItem">Home</li>
+        <li class="navListItem">Home</li>
+        <li class="navListItem">Home</li>
+        <li class="navListItem">Log in</li>
+    </ul>
+</div>
+</div>` + headerContainer.innerHTML;
