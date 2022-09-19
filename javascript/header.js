55cf8ecce31c124d557b1d94fe1dd912d5247d78
Eldin Kecap <eldinkecap@gmail.com>
2022-09-19 21:09:18 +0200

    Added front end for a login page

--- javascript/header.js
+++ javascript/header.js
@@ -5,10 +5,12 @@ headerContainer.innerHTML = `<div class="headerBar">
 </div>
 <div class="nav">
     <ul class="navList">
+        <li class="navListItem"><a href="./index.html">Home</a></li>
+        <li class="navListItem"><a href="#aboutHeading">About</a></li>
         <li class="navListItem">Home</li>
-        <li class="navListItem">Home</li>
-        <li class="navListItem">Home</li>
-        <li class="navListItem">Log in</li>
+        <li class="navListItem"><a href='./login.html'>Log in</a></li>
     </ul>
 </div>
 </div>` + headerContainer.innerHTML;
+
+titleContainer.innerHTML += '<title>Hotel Management</title><link rel="icon" type="image/x-icon" href="../images/Hotel-icon - Copy.png">'
