a2d7080d78fc7325e0598540f07a5d482e17b36a
Eldin Kecap <eldinkecap@gmail.com>
2022-11-12 21:21:05 +0100

    Added inserting to users table

--- javascriptReq/createAccountReq.js
+++ javascriptReq/createAccountReq.js
@@ -13,8 +13,11 @@ app.post('/createAccount',(req,res)=>{
     let { createAccountInformation } = req.body;
     console.log(createAccountInformation);
     conn.execute('INSERT INTO customer(first_name,last_name) VALUES(?,?)',[createAccountInformation.firstName,createAccountInformation.lastName],(err,res,field)=>{
-        console.log(res);
-        console.log(field);
+        // console.log(res);
+        conn.execute('INSERT INTO user(username,password,customer_id) VALUES(?,?,?)',[createAccountInformation.username,createAccountInformation.password,res.insertId],(err,res)=>{
+            console.log(res);
+        })
+        // console.log(field);
     });
     res.json({ success : true })
 })
