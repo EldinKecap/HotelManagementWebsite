880eb58d6f12c848f959d60881d107af55a26882
Eldin Kecap <eldinkecap@gmail.com>
2022-11-18 00:11:05 +0100

    trying to implement MVC

--- controller/createNewAccountController.js
+++ controller/createNewAccountController.js
@@ -1,15 +1,4 @@
-const express = require('express');
-const conn = require('./database');
-const cors = require('cors');
-let app = express();
-
-
-app.use(express.json());
-app.use(cors());
-
-
-
-app.post('/createAccount',(req,res)=>{
+const createNewAccount = (req,res)=>{
     let { createAccountInformation } = req.body;
     console.log(createAccountInformation);
     conn.execute('INSERT INTO customer(first_name,last_name) VALUES(?,?)',[createAccountInformation.firstName,createAccountInformation.lastName],(err,res,field)=>{
@@ -20,17 +9,6 @@ app.post('/createAccount',(req,res)=>{
         // console.log(field);
     });
     res.json({ success : true })
-})
+}
 
-
-// conn.connect(function(err){
-//     if (err) {
-//         throw err;
-//     }
-//     console.log('connected');
-// })
-
-
-    app.listen(5000,()=>{
-        console.log('Server is listening on port 5000...');
-    })
+module.exports = createNewAccount;
