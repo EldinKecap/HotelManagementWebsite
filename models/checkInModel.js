005f8447ffc4a05b9aaf2d43d8f03a03057c890e
Eldin Kecap <eldinkecap@gmail.com>
2023-03-14 18:43:37 +0100

    added sql query in checkIn Model

--- models/checkInModel.js
+++ models/checkInModel.js
@@ -1,8 +1,13 @@
+const conn = require("./database");
 
 
 let checkInModel = {};
 
-checkInModel.readAll = () => {
+checkInModel.readAll = (cb) => {
+    let sql = "SELECT * FROM user_room;"
+    let result = conn.execute(sql,(error, result, fields)=>{
+        cb(result);
+    });
 
 }
 
