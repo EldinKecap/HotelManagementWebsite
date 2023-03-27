1d1be7f8e235170aa2148ab707e19651314930af
Eldin Kecap <eldinkecap@gmail.com>
2023-03-27 14:41:16 +0200

    finished add room type frontend

---
+++ public/javascript/addTypeOfRoom.js
@@ -0,0 +1,36 @@
+
+function createTypeOfRoom() {
+    let submitButton = document.getElementById('createTypeOfRoom');
+    let data = {};
+    let error = document.getElementById('error')
+    console.log(submitButton.disabled);
+
+    submitButton.addEventListener('click',async () => {
+        data.type_of_room = document.getElementById('typeOfRoom').value;
+        data.price = document.getElementById('price').value;
+        console.log(data);
+        if (data.type_of_room && data.price) {
+            submitButton.disabled = true;
+            let response = await fetch('http://localhost:5000/typeOfRoom/create', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            let responseData = await response.json();
+            console.log(responseData);
+            if (responseData.msg == 'error') {
+                error.innerText = 'Room type already exists';
+            } else {
+                error.innerText = responseData.msg;
+            }
+            submitButton.disabled = false;
+        }else{
+            error.innerText = 'Data cant be empty or 0';
+        }
+    })
+
+}
+
+createTypeOfRoom();
